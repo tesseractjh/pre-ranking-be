3860bc55a5b418feac470740297cc28fed3948ab
Tesseract <tensai3355@gmail.com>
2022-11-25 03:59:53 +0900

    :art: jwt 검증 로직 분리

--- src/middlewares/verifySignupToken.ts
+++ src/middlewares/verifySignupToken.ts
@@ -1,16 +1,20 @@
+import dotenv from 'dotenv';
 import { RequestHandler } from 'express';
-import { CustomError } from '@middlewares/handleError';
+import jwtOption from '@config/jwt';
+import verifyToken from '@utils/verifyToken';
+
+dotenv.config();
+const { JWT_SECRET } = process.env;
 
 const verifySignupToken: RequestHandler = async (req, res, next) => {
   const { signup } = req.signedCookies;
+  const verify = verifyToken(400, '회원가입을 위한 임시 토큰이 유효하지 않음', {
+    redirect: '/login'
+  });
 
-  if (!signup || !signup.userId) {
-    throw new CustomError(400, '회원가입을 위한 임시 토큰이 유효하지 않음', {
-      redirect: '/login'
-    });
-  }
+  const { userId } = verify(signup, JWT_SECRET, jwtOption.SIGNUP_VERIFY);
+  req.signup = { userId };
 
-  req.signup = signup;
   next();
 };
 
---
+++ src/utils/verifyToken.ts
@@ -0,0 +1,18 @@
+import { CustomError } from '@middlewares/handleError';
+import jwt from 'jsonwebtoken';
+
+const verifyToken =
+  (...args: ConstructorParameters<typeof CustomError>) =>
+  (token: string, secret: jwt.Secret, options: jwt.VerifyOptions) => {
+    try {
+      const payload = jwt.verify(token, secret, options);
+      if (typeof payload === 'string' || 'payload' in payload) {
+        throw new Error();
+      }
+      return payload;
+    } catch {
+      throw new CustomError(...args);
+    }
+  };
+
+export default verifyToken;
